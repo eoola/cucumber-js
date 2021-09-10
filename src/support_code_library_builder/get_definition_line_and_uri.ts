18bd3b03363f230c74b787a175f2334e55f515bd
Emmanuel Ola <54866720+eoola@users.noreply.github.com>
2021-09-10 12:52:42 -0400

    attempt to read source map

--- src/support_code_library_builder/get_definition_line_and_uri.ts
+++ src/support_code_library_builder/get_definition_line_and_uri.ts
@@ -4,6 +4,9 @@ import { isFileNameInCucumber } from '../stack_trace_filter'
 import { doesHaveValue, valueOrDefault } from '../value_checker'
 import { ILineAndUri } from '../types'
 import CallSite = NodeJS.CallSite
+import { fs } from 'mz'
+import { SourceMapConsumer } from 'source-map'
+import { SourceMap } from 'module'
 
 export function getDefinitionLineAndUri(
   cwd: string,
@@ -12,16 +15,54 @@ export function getDefinitionLineAndUri(
   let line: number
   let uri: string
   try {
+
+    var origPrepareStackTrace = Error.prepareStackTrace
+
+    Error.prepareStackTrace = function(_, stack) {
+      return stack
+    }
+
+    var err = new Error()
+
+    var stack = err.stack as unknown as Array<CallSite>
+
+    var filename = stack[1].getFileName()
+
+    var source = fs.readFileSync(filename, 'utf8')
+
+    var sourceMappingUrlRegExp = /\/\/[#@] ?sourceMappingURL=([^\s'"]+)\s*$/mg;
+    let lastSourceMappingUrl;
+    let matchSourceMappingUrl;
+
+    while(matchSourceMappingUrl = sourceMappingUrlRegExp.exec(source)) {
+      lastSourceMappingUrl = matchSourceMappingUrl[1];
+    }
+
+    if (lastSourceMappingUrl) {
+      let sourceMappingUrl = lastSourceMappingUrl
+      let isDataUrl = sourceMappingUrl.substr(0,5) === 'data:';
+      let defaultSourceRoot = filename.substring(0, filename.lastIndexOf('/') + 1);
+
+      let sourceMap = fs.readFileSync(sourceMappingUrl, 'utf8')
+
+      //let sourceConsumer = new SourceMapConsumer(sourceMap);
+    }
+
+    console.log(stack)
+
+    /*
     console.log(new Error().stack.split("\n").slice(1).map(
       (line: String) => line.match(/(\/.*):(\d+):\d+/).slice(1,3)
     ))
-    const stackframes = stackChain.callSite()
-    /*console.log(
+    */
+
+    const stackframes = stack
+    console.log(
       stackframes.map((frame: CallSite) => [
         frame.getLineNumber(),
         frame.getFileName(),
       ])
-    )*/
+    )
     const stackframe = stackframes.find(
       (frame: CallSite) =>
         frame.getFileName() !== __filename && !isExcluded(frame.getFileName())
@@ -41,3 +82,5 @@ export function getDefinitionLineAndUri(
     uri: valueOrDefault(uri, 'unknown'),
   }
 }
+
+
